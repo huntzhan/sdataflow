dfdf4ab09cf8bfba49a170a0c09b49208a804c86
haoxun <programmer.zhx@gmail.com>
2015-04-23 23:11:56 +0800

    Refactor analysis.py.

--- sdataflow/lang/analysis.py
+++ sdataflow/lang/analysis.py
@@ -12,8 +12,17 @@ class DataFlow(object):
         self.entry_table = {}
         self.outcome_type_table = {}
 
-    def _retrieve_or_record_element(self, table_name, element):
-        element_table = getattr(self, table_name)
+    def get_unique_element(self, element):
+        # target table.
+        element_table = None
+        if isinstance(element, Entry):
+            element_table = self.entry_table
+        elif isinstance(element, OutcomeType):
+            element_table = self.outcome_type_table
+        else:
+            raise RuntimeError('get_unique_element')
+
+        # retrieve or register element.
         if element.name in element_table:
             # retrieve.(ensure uniqueness)
             return element_table[element.name]
@@ -22,30 +31,20 @@ class DataFlow(object):
             element_table[element.name] = element
             return element
 
-    def get_entry(self, entry):
-        return self._retrieve_or_record_element('entry_table', entry)
-
-    def get_outcome_type(self, entry):
-        return self._retrieve_or_record_element('outcome_type_table', entry)
-
     def build_DAG(self):
         for src, dst in self.rules:
+            src = self.get_unique_element(src)
+            dst = self.get_unique_element(dst)
             if isinstance(src, Entry) and isinstance(dst, Entry):
-                src = self.get_entry(src)
-                dst = self.get_entry(dst)
                 # transform `EntryA --> EntryB` to
                 # `EntryA --> [EntryA]` and `[EntryA] --> EntryB`.
-                outcome_type = self.get_outcome_type(OutcomeType(src.name))
+                outcome_type = self.get_unique_element(OutcomeType(src.name))
                 src.add_outcome_type(outcome_type)
                 outcome_type.add_entry(dst)
             elif isinstance(src, Entry) and isinstance(dst, OutcomeType):
-                src = self.get_entry(src)
-                dst = self.get_outcome_type(dst)
                 # add outcome_type to entry.
                 src.add_outcome_type(dst)
             elif isinstance(src, OutcomeType) and isinstance(dst, Entry):
-                src = self.get_outcome_type(src)
-                dst = self.get_entry(dst)
                 # add entry to outcome_type.
                 src.add_entry(dst)
             else:
