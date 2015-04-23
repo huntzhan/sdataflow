9a33ea22b1caeef69ba6bb2dc9903c0661ddde02
haoxun <programmer.zhx@gmail.com>
2015-04-23 19:00:06 +0800

    Removed cached code gen.

--- sdataflow/lang/generated_lextab.py
+++
@@ -1,9 +0,0 @@
-# generated_lextab.py. This file automatically created by PLY (version 3.4). Don't edit!
-_tabversion   = '3.4'
-_lextokens    = {u'BRACKET_LEFT': 1, u'DOUBLE_HYPHENS': 1, u'BRACKET_RIGHT': 1, u'ARROW': 1, u'ID': 1}
-_lexreflags   = 32
-_lexliterals  = ''
-_lexstateinfo = {'INITIAL': 'inclusive'}
-_lexstatere   = {'INITIAL': [(u'(?P<t_newline>\\n+)|(?P<t_ARROW>\\-\\-\\>)|(?P<t_ignore_COMMENT>\\#.*)|(?P<t_DOUBLE_HYPHENS>\\-\\-)|(?P<t_ID>\\w+)|(?P<t_ignore_WHITESPACE>\\s)|(?P<t_BRACKET_LEFT>\\[)|(?P<t_BRACKET_RIGHT>\\])', [None, (u't_newline', 'newline'), (None, 'ARROW'), (None, None), (None, 'DOUBLE_HYPHENS'), (None, 'ID'), (None, None), (None, 'BRACKET_LEFT'), (None, 'BRACKET_RIGHT')])]}
-_lexstateignore = {'INITIAL': ''}
-_lexstateerrorf = {'INITIAL': 't_error'}
